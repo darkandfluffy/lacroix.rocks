eb973f71737d727c132a08c2af042ab0cacda07b
darkandfluffy <56565336+darkandfluffy@users.noreply.github.com>
2019-10-14 19:01:10 -0400

    Spelling

--- src/components/FactViewer.vue
+++ src/components/FactViewer.vue
@@ -50,7 +50,7 @@ export default {
         2: `LaCroix has often been likened to "an orgasm in a can".`,
         3: `LaCroix tastes like freedom unleashed.`,
         4: `LaCroix is single-handedly saving the pandas from extinction. 🐼`,
-        5: `"Pamplemousse" is a funny word.`
+        5: `"Pamplemousse" is French for "Grapefruit".`
       },
       currentFact: 0
     }
